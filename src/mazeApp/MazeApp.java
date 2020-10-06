ce8179f09f8bb0284ac93e7eb170300d1e77b01e
sebobrien <sebobrien@gmail.com>
2020-10-06 15:17:49 +0200

    clean up main

--- src/mazeApp/MazeApp.java
+++ src/mazeApp/MazeApp.java
@@ -15,28 +15,7 @@ public class MazeApp {
 	public static void main(String[] args) {
 		//Maze maze = (new BitSetMaze(10, 50, 0, 9));
 		//System.out.println(maze.toString());
-//		DisjointSet test = new CompositeUpTree(10);
-//		System.out.println(test.toString());
-//		test.union(test.find(0),test.find(8));
-//		System.out.println(test.toString());
-//		test.union(test.find(9),test.find(1));
-//		System.out.println(test.toString());
-//		test.union(test.find(8),test.find(6));
-//		System.out.println(test.toString());
-//		test.union(test.find(9),test.find(0));
-//		System.out.println(test.toString());
-//		test.union(test.find(2),test.find(3));
-//		System.out.println(test.toString());
-//		test.union(test.find(5),test.find(4));
-//		System.out.println(test.toString());
-//		test.union(test.find(2),test.find(5));
-//		System.out.println(test.toString());
-//		test.union(test.find(6),test.find(4));
-//		System.out.println(test.toString());
-//		test.union(test.find(7),test.find(1));
-//		System.out.println(test.toString());
-//		System.out.println(test.find(4));
-//		System.out.println(test.toString());
+
 		 
 	
 		MazeGenerator mazeGen = new DisjointSetMazeGen(40, 40, 0, 2); 
